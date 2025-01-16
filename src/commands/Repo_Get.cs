cc24575cfb29ff5b999ddca24fb2dc91de5cc5e7
threddyrex <threddy@threddyrex.com>
2025-01-16 13:42:51 -0800

    let user pass in handle

--- src/commands/Repo_Get.cs
+++ src/commands/Repo_Get.cs
@@ -6,73 +6,71 @@ using System.Text.Json;
 using System.Text.Json.Nodes;
 using dnproto.utils;
 
-namespace dnproto.commands
+namespace dnproto.commands;
+public class Repo_Get : BaseCommand
 {
-    public class Repo_Get : BaseCommand
+    public override HashSet<string> GetRequiredArguments()
     {
-        public override HashSet<string> GetRequiredArguments()
+        return new HashSet<string>(new string[]{"repoFile"});
+    }
+
+    public override HashSet<string> GetOptionalArguments()
+    {
+        return new HashSet<string>(new string[]{"handle", "pds", "did"});
+    }
+
+
+    /// <summary>
+    /// Downloads a user's repository.
+    /// </summary>
+    /// <param name="arguments"></param>
+    /// <exception cref="ArgumentException"></exception>
+    public override void DoCommand(Dictionary<string, string> arguments)
+    {
+        //
+        // Get arguments
+        //
+        string? pds = CommandLineInterface.GetArgumentValue(arguments, "pds");
+        string? did = CommandLineInterface.GetArgumentValue(arguments, "did");
+        string? handle = CommandLineInterface.GetArgumentValue(arguments, "handle");
+        string? repoFile = CommandLineInterface.GetArgumentValue(arguments, "repoFile");
+
+        Console.WriteLine($"pds: {pds}");
+        Console.WriteLine($"did: {did}");
+        Console.WriteLine($"repoFile: {repoFile}");
+
+        //
+        // If we're resolving handle, do that now.
+        //
+        if(string.IsNullOrEmpty(handle) == false)
         {
-            return new HashSet<string>(new string[]{"repoFile"});
+            Console.WriteLine("Resolving handle to did.");
+            Dictionary<string, string> handleInfo = Handle_ResolveInfo.DoResolveHandleInfo(handle);
+
+            did = handleInfo.ContainsKey("did") ? handleInfo["did"] : null;
+            pds = handleInfo.ContainsKey("pds") ? handleInfo["pds"] : null;
         }
 
-        public override HashSet<string> GetOptionalArguments()
+        Console.WriteLine($"pds: {pds}");
+        Console.WriteLine($"did: {did}");
+
+        string url = $"https://{pds}/xrpc/com.atproto.sync.getRepo?did={did}";
+        Console.WriteLine($"url: {url}");
+
+        if (string.IsNullOrEmpty(pds) || string.IsNullOrEmpty(did) || string.IsNullOrEmpty(repoFile))
         {
-            return new HashSet<string>(new string[]{"handle", "pds", "did"});
+            Console.WriteLine("Invalid arguments.");
+            return;
         }
 
 
-        /// <summary>
-        /// Downloads a user's repository.
-        /// </summary>
-        /// <param name="arguments"></param>
-        /// <exception cref="ArgumentException"></exception>
-        public override void DoCommand(Dictionary<string, string> arguments)
-        {
-            //
-            // Get arguments
-            //
-            string? pds = CommandLineInterface.GetArgumentValue(arguments, "pds");
-            string? did = CommandLineInterface.GetArgumentValue(arguments, "did");
-            string? handle = CommandLineInterface.GetArgumentValue(arguments, "handle");
-            string? repoFile = CommandLineInterface.GetArgumentValue(arguments, "repoFile");
+        //
+        // Call WS
+        //
+        WebServiceClient.SendRequest(url,
+            HttpMethod.Get, 
+            outputFilePath: repoFile,
+            parseJsonResponse: false);
 
-            Console.WriteLine($"pds: {pds}");
-            Console.WriteLine($"did: {did}");
-            Console.WriteLine($"repoFile: {repoFile}");
-
-            //
-            // If we're resolving handle, do that now.
-            //
-            if(string.IsNullOrEmpty(handle) == false)
-            {
-                Console.WriteLine("Resolving handle to did.");
-                Dictionary<string, string> handleInfo = Handle_ResolveInfo.DoResolveHandleInfo(handle);
-
-                did = handleInfo.ContainsKey("did") ? handleInfo["did"] : null;
-                pds = handleInfo.ContainsKey("pds") ? handleInfo["pds"] : null;
-            }
-
-            Console.WriteLine($"pds: {pds}");
-            Console.WriteLine($"did: {did}");
-
-            string url = $"https://{pds}/xrpc/com.atproto.sync.getRepo?did={did}";
-            Console.WriteLine($"url: {url}");
-
-            if (string.IsNullOrEmpty(pds) || string.IsNullOrEmpty(did) || string.IsNullOrEmpty(repoFile))
-            {
-                Console.WriteLine("Invalid arguments.");
-                return;
-            }
-
-
-            //
-            // Call WS
-            //
-            WebServiceClient.SendRequest(url,
-                HttpMethod.Get, 
-                outputFilePath: repoFile,
-                parseJsonResponse: false);
-
-        }
     }
 }
--- src/commands/Repo_GetStatus.cs
+++ src/commands/Repo_GetStatus.cs
@@ -10,12 +10,12 @@ public class Repo_GetStatus : BaseCommand
 {
     public override HashSet<string> GetRequiredArguments()
     {
-        return new HashSet<string>(new string[]{"did"});
+        return new HashSet<string>(new string[]{});
     }
 
     public override HashSet<string> GetOptionalArguments()
     {
-        return new HashSet<string>(new string[]{"pds", "outfile"});
+        return new HashSet<string>(new string[]{"handle", "did", "pds", "outfile"});
     }
 
 
@@ -26,14 +26,43 @@ public class Repo_GetStatus : BaseCommand
     /// <exception cref="ArgumentException"></exception>
     public override void DoCommand(Dictionary<string, string> arguments)
     {
-        string pds = arguments.ContainsKey("pds") ? arguments["pds"] : "bsky.social";
-        string did = arguments["did"];
-        string url = $"https://{pds}/xrpc/com.atproto.sync.getRepoStatus?did={did}";
+        string? pds = CommandLineInterface.GetArgumentValue(arguments, "pds");
+        string? did = CommandLineInterface.GetArgumentValue(arguments, "did");
+        string? handle = CommandLineInterface.GetArgumentValue(arguments, "handle");
 
         Console.WriteLine($"pds: {pds}");
         Console.WriteLine($"did: {did}");
+
+
+        //
+        // If we're resolving handle, do that now.
+        //
+        if(string.IsNullOrEmpty(handle) == false)
+        {
+            Console.WriteLine("Resolving handle to did.");
+            Dictionary<string, string> handleInfo = Handle_ResolveInfo.DoResolveHandleInfo(handle);
+
+            Console.WriteLine(JsonData.ConvertObjectToJsonString(handleInfo));
+            Console.WriteLine();
+
+            did = handleInfo.ContainsKey("did") ? handleInfo["did"] : null;
+            pds = handleInfo.ContainsKey("pds") ? handleInfo["pds"] : null;
+        }
+
+        Console.WriteLine($"pds: {pds}");
+        Console.WriteLine($"did: {did}");
+
+        string url = $"https://{pds}/xrpc/com.atproto.sync.getRepoStatus?did={did}";
         Console.WriteLine($"url: {url}");
 
+        if (string.IsNullOrEmpty(pds) || string.IsNullOrEmpty(did))
+        {
+            Console.WriteLine("Invalid arguments.");
+            return;
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("Calling getRepoStatus.");
         JsonNode? repoStatus = WebServiceClient.SendRequest(url,
             HttpMethod.Get);
 
