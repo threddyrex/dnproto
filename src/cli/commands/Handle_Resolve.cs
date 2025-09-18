02956fa033c8db25a7cb6bc38f3a534c70e79b8d
robtougher <rob@robtougher.com>
2025-09-18 15:50:03 -0700

    simplify resolve command

--- src/cli/commands/Handle_Resolve.cs
+++ src/cli/commands/Handle_Resolve.cs
@@ -13,11 +13,6 @@ public class Handle_Resolve : BaseCommand
         return new HashSet<string>(new string[]{"handle"});
     }
 
-    public override HashSet<string> GetOptionalArguments()
-    {
-        return new HashSet<string>(new string[]{"outfile"});
-    }
-
 
     /// <summary>
     /// Resolves a handle to a JSON object.
@@ -26,27 +21,8 @@ public class Handle_Resolve : BaseCommand
     /// <exception cref="ArgumentException"></exception>
     public override void DoCommand(Dictionary<string, string> arguments)
     {
-        //
-        // Get arguments.
-        //
-        if(arguments.ContainsKey("handle") == false)
-        {
-            throw new ArgumentException("Missing required argument: handle");
-        }
-
         string? did = BlueskyClient.ResolveHandleToDid_ViaBlueskyApi(arguments["handle"]);
-        Logger.LogInfo($"Resolved DID: {did}");
-
-        //
-        // Print response.
-        //
-        string? outfile = CommandLineInterface.GetArgumentValue(arguments, "outfile");
-        if(string.IsNullOrEmpty(outfile) == false)
-        {
-            Console.WriteLine($"Writing response to file: {outfile}");
-            File.WriteAllText(outfile, did ?? "");
-        }
-
+        Logger.LogInfo($"{did}");
     }
 
 }
