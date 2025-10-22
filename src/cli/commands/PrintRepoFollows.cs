99b8924b8dfcea1fd59eefa3bd164493a18de83f
robtougher <rob@robtougher.com>
2025-10-22 13:48:24 -0700

    print follows

---
+++ src/cli/commands/PrintRepoFollows.cs
@@ -0,0 +1,94 @@
+using System.Text;
+
+using dnproto.repo;
+
+namespace dnproto.cli.commands
+{
+    public class PrintRepoFollows : BaseCommand
+    {
+        public override HashSet<string> GetRequiredArguments()
+        {
+            return new HashSet<string>(new string[]{"dataDir", "handle"});
+        }
+
+        public override HashSet<string> GetOptionalArguments()
+        {
+            return new HashSet<string>(new string[]{"month"});
+        }
+
+        public override void DoCommand(Dictionary<string, string> arguments)
+        {
+            //
+            // Get arguments
+            //
+            string? dataDir = CommandLineInterface.GetArgumentValue(arguments, "dataDir");
+            string? handle = CommandLineInterface.GetArgumentValue(arguments, "handle");
+            string? month = CommandLineInterface.GetArgumentValue(arguments, "month");
+
+            //
+            // Get local path of repo file (assumes user called GetRepo first to pull it down).
+            //
+            string? repoFile = LocalFileSystem.Initialize(dataDir, Logger)?.GetPath_RepoFile(handle);
+            if (string.IsNullOrEmpty(repoFile) || File.Exists(repoFile) == false)
+            {
+                Logger.LogError($"Repo file does not exist: {repoFile}");
+                return;
+            }
+
+            //
+            // Walk repo
+            //
+            List<RepoRecord> follows = new List<RepoRecord>();
+
+            Repo.WalkRepo(
+                repoFile,
+                (repoHeader) =>
+                {
+                    return true;
+                },
+                (repoRecord) =>
+                {
+                    if (string.IsNullOrEmpty(repoRecord.RecordType)) return true;
+                    if (string.Equals(repoRecord.RecordType, "app.bsky.graph.follow", StringComparison.OrdinalIgnoreCase) == false) return true;
+
+
+                    if (string.IsNullOrEmpty(month) == false)
+                    {
+                        if (DateTime.TryParse(repoRecord.CreatedAt, out DateTime createdAt))
+                        {
+                            string postMonth = createdAt.ToString("yyyy-MM");
+                            if (month.Equals(postMonth))
+                            {
+                                follows.Add(repoRecord);
+                            }
+                        }
+                    }
+                    else
+                    {
+                        follows.Add(repoRecord);
+                    }
+
+                    return true;
+                }
+            );
+
+
+            //
+            // Print, sorted
+            //
+            var sortedFollows = follows.OrderBy(fr => fr.DataBlock.SelectString(["createdAt"]));
+            foreach (var repoRecord in sortedFollows)
+            {
+                Logger.LogInfo($"[{repoRecord.DataBlock.SelectString(["createdAt"])}] https://bsky.app/profile/{repoRecord.DataBlock.SelectString(["subject"])}");
+
+                //
+                // Print text content
+                //
+                string? text = repoRecord.JsonString;
+                Logger.LogTrace(text);
+            }
+
+            Logger.LogInfo($"Total follows found: {follows.Count}");
+        }
+   }
+}
