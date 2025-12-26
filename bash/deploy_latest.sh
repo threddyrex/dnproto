77e3d838ee4a73b396a8ebbf005924e7c4c55444
threddyrex <threddy@threddyrex.com>
2025-12-26 17:51:10 +0000

    bash env file

--- bash/deploy_latest.sh
+++ bash/deploy_latest.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
+source ../data/pds/setup.env
+
+echo "PDS_HOST_NAME: $PDS_HOST_NAME"
+echo "AVAILABLE_USER_DOMAINS: $AVAILABLE_USER_DOMAINS"
+echo "USER_HANDLE: $USER_HANDLE"
+echo "USER_DID: $USER_DID"
+echo "USER_EMAIL: $USER_EMAIL"
+echo "SYSTEMCTL_SERVICE_NAME: $SYSTEMCTL_SERVICE_NAME"
+
+cd ..
 
 git pull
 dotnet build
-systemctl restart dnproto.threddyrex.net.service
+systemctl restart $SYSTEMCTL_SERVICE_NAME
+
+cd bash/
