2658b98fb5cd604b63a1652f2921ac0873786964
threddyrex <threddy@threddyrex.com>
2025-12-26 17:56:34 +0000

    env file for bash

--- bash/deploy_latest.sh
+++ bash/deploy_latest.sh
@@ -2,17 +2,29 @@
 
 source ../data/pds/setup.env
 
-echo "PDS_HOST_NAME: $PDS_HOST_NAME"
-echo "AVAILABLE_USER_DOMAINS: $AVAILABLE_USER_DOMAINS"
-echo "USER_HANDLE: $USER_HANDLE"
-echo "USER_DID: $USER_DID"
-echo "USER_EMAIL: $USER_EMAIL"
-echo "SYSTEMCTL_SERVICE_NAME: $SYSTEMCTL_SERVICE_NAME"
+echo ""
+echo "CONFIG FROM ../data/pds/setup.env:"
+echo ""
+echo "	PDS_HOST_NAME: $PDS_HOST_NAME"
+echo "	AVAILABLE_USER_DOMAIN: $AVAILABLE_USER_DOMAIN"
+echo "	USER_HANDLE: $USER_HANDLE"
+echo "	USER_DID: $USER_DID"
+echo "	USER_EMAIL: $USER_EMAIL"
+echo "	SYSTEMCTL_SERVICE_NAME: $SYSTEMCTL_SERVICE_NAME"
+echo ""
 
 cd ..
 
+echo "GIT PULL"
 git pull
+echo ""
+
+echo "DOTNET BUILD"
 dotnet build
+echo ""
+
+echo "SYSTEMCTL RESTART"
 systemctl restart $SYSTEMCTL_SERVICE_NAME
+echo ""
 
 cd bash/
--- bash/initialize_pds.sh
+++ bash/initialize_pds.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
 
+ource ../data/pds/setup.env
 
-../src/bin/Debug/net10.0/dnproto /command InitializePds /datadir ../data/ /pdshostname dnproto.threddyrex.net /availableuserdomain ".threddyrex.net" /userhandle "threddyrex.net" /userdid "did:web:threddyrex.net" /useremail "threddy+dn@threddyrex.com
+echo ""
+echo "CONFIG FROM ../data/pds/setup.env:"
+echo ""
+echo "  PDS_HOST_NAME: $PDS_HOST_NAME"
+echo "  AVAILABLE_USER_DOMAIN: $AVAILABLE_USER_DOMAIN"
+echo "  USER_HANDLE: $USER_HANDLE"
+echo "  USER_DID: $USER_DID"
+echo "  USER_EMAIL: $USER_EMAIL"
+echo "  SYSTEMCTL_SERVICE_NAME: $SYSTEMCTL_SERVICE_NAME"
+echo ""
+
+cd ..
+
+
+
+../src/bin/Debug/net10.0/dnproto /command InitializePds /datadir ../data/ /pdshostname $PDS_HOST_NAME /availableuserdomain $AVAILABLE_USER_DOMAIN /userhandle $USER_HANDLE /userdid $USER_DID /useremail $USER_EMAIL
 
