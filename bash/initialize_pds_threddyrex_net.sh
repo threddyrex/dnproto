4b55c03005443d5194b68b34097e7362685e2596
threddyrex <threddy@threddyrex.com>
2025-12-26 07:21:14 +0000

    new pds name

--- bash/initialize_pds_threddyrex_net.sh
+++ bash/initialize_pds_threddyrex_net.sh
@@ -2,5 +2,5 @@
 
 
 
-../src/bin/Debug/net10.0/dnproto /command InitializePds /datadir ../data/ /pdshostname pds.dnproto.com /availableuserdomain ".threddyrex.net" /userhandle "threddyrex.net" /userdid "did:web:threddyrex.net" /useremail "threddy+dn@threddyrex.com
+../src/bin/Debug/net10.0/dnproto /command InitializePds /datadir ../data/ /pdshostname dnproto.threddyrex.net /availableuserdomain ".threddyrex.net" /userhandle "threddyrex.net" /userdid "did:web:threddyrex.net" /useremail "threddy+dn@threddyrex.com
 
