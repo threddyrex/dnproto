92d1b4e38edb49391fbd38c6c13491255a79527b
threddyrex <threddy@threddyrex.com>
2026-01-13 10:59:16 -0800

    bash scripts for log level

---
+++ bash/set_log_level_trace.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+source ../data/pds/setup.env
+
+echo ""
+echo "CONFIG FROM ../data/pds/setup.env:"
+echo ""
+echo "  PDS_HOST_NAME: $PDS_HOST_NAME"
+echo "  AVAILABLE_USER_DOMAIN: $AVAILABLE_USER_DOMAIN"
+echo "  USER_HANDLE: $USER_HANDLE"
+echo "  USER_DID: $USER_DID"
+echo "  USER_EMAIL: $USER_EMAIL"
+echo "  SYSTEMCTL_SERVICE_NAME: $SYSTEMCTL_SERVICE_NAME"
+echo ""
+
+
+
+../src/bin/Debug/net10.0/dnproto /command SetLogLevel /dataDir ../data/ /newLevel Trace
