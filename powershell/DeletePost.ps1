b8a98da49ff55598c65ba4dd0dd56cb309ca3fa0
threddyrex <threddy@threddyrex.com>
2026-01-26 11:06:50 -0800

    deletepost ps

--- powershell/DeletePost.ps1
+++ powershell/DeletePost.ps1
@@ -2,11 +2,12 @@ param (
     [string]$dnprotoPath = $null,
     [string]$logLevel = $null,
     [string]$dataDir = $null,
-    [Parameter(Mandatory=$true)]
-    [string]$uri
+    [string]$actor = $null,
+    [Parameter(Mandatory=$true, Position=0)]
+    [string]$url
 )
 
 . .\_Defaults.ps1
 
 # call dnproto.exe to get handle info
-& $dnprotoPath /command DeletePost /uri $uri /logLevel $logLevel /dataDir $dataDir
+& $dnprotoPath /command DeletePost /url $url /logLevel $logLevel /dataDir $dataDir /actor $actor
--- powershell/_Defaults.ps1
+++ powershell/_Defaults.ps1
@@ -4,6 +4,6 @@
 if(-not $dnprotoPath) { $dnprotoPath = "..\src\bin\Debug\net10.0\dnproto.exe" }
 if(-not $logLevel) { $logLevel = "Info" }
 if(-not $dataDir) { $dataDir = "..\data\" }
-if(-not $actor) { $actor = "threddyrex.com" }
+if(-not $actor) { $actor = "threddyrex.org" }
 
 
